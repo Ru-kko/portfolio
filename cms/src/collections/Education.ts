f8c7d090990da5136c46312799af663ad76a1648
Ru-kko <verase2003.1@gmail.com>
2023-12-12 19:22:32 -0500

    feat: Education collection

---
+++ cms/src/collections/Education.ts
@@ -0,0 +1,56 @@
+import { CollectionConfig } from "payload/types";
+
+const Education: CollectionConfig = {
+  slug: "education",
+  admin: {
+    useAsTitle: "name",
+  },
+  fields: [
+    {
+      name: "title",
+      type: "text",
+    },
+    {
+      name: "from",
+      type: "text",
+    },
+    {
+      name: "description",
+      type: "richText",
+    },
+    {
+      name: "start_date",
+      type: "date",
+    },
+    {
+      name: "end_date",
+      type: "date",
+      required: false,
+      validate: (val: Date, { data }: { data: { start_date: Date } }) => {
+        if (val <= data.start_date)
+          return "End date should be after start date";
+        return true;
+      },
+    },
+    {
+      name: "type",
+      type: "select",
+      options: [
+        {
+          label: "Certificate",
+          value: "certificate",
+        },
+        {
+          label: "Degree",
+          value: "degree"
+        },
+        {
+          label: "Course",
+          value: "course"
+        }
+      ]
+    }
+  ],
+};
+
+export { Education };
