efabfd639c80565e74c098ec5343c471910683cc
Ru-kko <verase2003.1@gmail.com>
2023-12-30 08:25:47 -0500

    feat(front): message post endpoint gateway before crm

---
+++ front/src/lib/graphql/Messages.ts
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+import { QueryFunction, client } from ".";
+import { MessageRequestArgs, id } from "@/types";
+
+const POST_MESSAGE = gql(`
+  mutation CreateMessage($msg: mutationMessageInput!) {
+    createMessage(data: $msg) {
+        id
+    }
+  }
+`);
+
+export const postMessage: QueryFunction<
+  id,
+  { msg: MessageRequestArgs }
+> = async ({ msg }) => {
+  const res = await client
+    .mutate<id>({ mutation: POST_MESSAGE, variables: { msg } })
+  return res.data;
+};
