a0c36d1e0690848622f6756f92787dfecfb3dba0
Ru-kko <verase2003.1@gmail.com>
2024-01-01 06:36:50 -0500

    feat(api): message gateway endpoint on the front

--- front/src/app/api/message/route.ts
+++ front/src/app/api/message/route.ts
@@ -1,9 +1,12 @@
+import { postMessage } from "@/lib/graphql";
+import { MessageRequestArgs } from "@/types/Message";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const cookie = req.cookies.get("last-message")?.value;
+  const nextTry = Date.now() + 1800000;
 
-  if (cookie && parseInt(cookie) <= Date.now() + 1800000) {
+  if (cookie && parseInt(cookie) <= nextTry) {
     return NextResponse.json(
       {
         error: "MessageTimeout",
@@ -15,21 +18,40 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  const body = await req.json().catch((e) => {
-    console.error(e);
-    return e;
-  });
+  const body: MessageRequestArgs | Error = await req
+    .json()
+    .then((r) => {
+      return {
+        title: r.title,
+        email: r.email,
+        content: r.content,
+      };
+    })
+    .catch((e) => e);
 
-  if (!(body.content && body.email && body.title)) {
+  if (body instanceof Error) {
+    console.error(body);
     return NextResponse.json(
       { error: "BadRequest", message: "Missing data" },
       { status: 400 }
     );
   }
 
-  console.log("Unimplemented");
-  return NextResponse.json(
-    { error: "NotImplemented" },
-    { status: 501 }
+  const gres = await postMessage({ msg: body }).catch((e: Error) => e);
+  if (!gres || gres instanceof Error) {
+    console.error(gres);
+    return NextResponse.json(
+      { error: "InternalServerError", message: gres?.message },
+      { status: 500 }
+    );
+  }
+
+  const response = NextResponse.json(
+    { status: "Ok", message: "succesfully message created" },
+    { status: 200 }
   );
+
+  response.cookies.set("last-message", nextTry.toString());
+
+  return response;
 }
