2f8936b8ff8f469ff0622c8401c2f6fcd4400b8c
Ru-kko <verase2003.1@gmail.com>
2024-01-06 13:07:25 -0500

    feat(front): Sortlist request service

--- front/src/services/impl/GraphqlApiService.ts
+++ front/src/services/impl/GraphqlApiService.ts
@@ -1,11 +1,27 @@
-import { MessageRequestArgs, id } from "@/types";
+import { ListResponse, MessageRequestArgs, id } from "@/types";
 import { ApiService } from "..";
-import { postMessage } from "@/lib/graphql";
+import { getEducationList, postMessage } from "@/lib/graphql";
+import { EducationBase } from "@/types/Education";
 
 export class GraphqlApiService implements ApiService {
   async sendMessage(msg: MessageRequestArgs): Promise<id> {
     const res = await postMessage({ msg });
 
     return res as id;
+  }
+
+  async getShortEducation(
+    page?: number
+  ): Promise<ListResponse<EducationBase>> {
+    const res = await getEducationList({ page: page ?? 1 });
+
+    return (
+      res ?? {
+        docs: [] as EducationBase[],
+        page: page ?? 1,
+        prevPage: page ? page - 1 : null,
+        totalPages: 0,
+      }
+    );
   }
 }
