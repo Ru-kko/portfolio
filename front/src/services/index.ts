e227422a65198fd8a69f479592ceb52cae9e7a57
Ru-kko <verase2003.1@gmail.com>
2024-01-07 12:53:36 -0500

    feat(front): add short education method on API interface

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -1,8 +1,9 @@
-import { MessageRequestArgs, id } from "@/types";
+import { EducationBase, ListResponse, MessageRequestArgs, id } from "@/types";
 import { GraphqlApiService } from "./impl/GraphqlApiService";
 
 export interface ApiService {
   sendMessage(msg: MessageRequestArgs): Promise<id>;
+  getShortEducation(page?: number): Promise<ListResponse<EducationBase>>;
 }
 
 export function getApiService(): ApiService {
