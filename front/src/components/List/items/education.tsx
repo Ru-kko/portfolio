0d0007d474c7f9b67d4f9c1020837509ce4e74bd
Ru-kko <verase2003.1@gmail.com>
2024-01-19 23:37:39 -0500

    refractor(front): move hover effect to shared styles

--- front/src/components/List/items/education.tsx
+++ front/src/components/List/items/education.tsx
@@ -2,16 +2,15 @@ import { EducationBase } from "@/types";
 import shared from "./css/shared.module.css";
 import style from "./css/education.module.css";
 import Link from "next/link";
-import { extendsClassName } from "@/utils/client";
 
 export function EducationItem(props: EducationBase) {
   return (
     <li className={shared.item}>
       <Link
         href={"/education/" + props.id}
-        className={extendsClassName(shared.background, style.link)}
+        className={shared.background}
       >
-        <h2 className={style.title}>{props.title}</h2>
+        <h2>{props.title}</h2>
         <div className={style.subtitle}>
           <h3>{props.from}</h3>
           <span className={style[props.type]}>
