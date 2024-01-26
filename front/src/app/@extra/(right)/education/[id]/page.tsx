a4ab0d14b1815531cb3279d20a48631278555f79
Ru-kko <verase2003.1@gmail.com>
2024-01-26 17:26:26 -0500

    feat(front): layout styles to right content

--- front/src/app/@extra/(right)/education/[id]/page.tsx
+++ front/src/app/@extra/(right)/education/[id]/page.tsx
@@ -1,3 +1,21 @@
-export default function DetailedEducationPage({ params }: { params: { id: string } }) {
-  return <>Detailed Education Page {params.id}</>;
+import { RichText } from "@/components/RichText";
+import { getApiService } from "@/services";
+import { ApolloError } from "@apollo/client";
+import { redirect } from "next/navigation";
+
+export default async function DetailedEducationPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  try {
+    const data = await getApiService().getFullEducationInfo(params.id);
+
+    return <RichText content={data.description} />;
+  } catch (e) {
+    if (!(e instanceof ApolloError && e.message !== "The requested resource was not found.")) {
+      console.error(e);
+    }
+    redirect("/");
+  }
 }
--- front/src/app/@extra/(right)/layout.tsx
+++ front/src/app/@extra/(right)/layout.tsx
@@ -1,5 +1,20 @@
 import { PropsWithChildren } from "react";
+import { HouseIcon } from "@/components";
+import { GoBackComponent } from "@/components/GoBack";
+import { extendsClassName } from "@/utils/client";
+import style from "./right.module.css";
+import Link from "next/link";
 
 export default function extraLayout({ children }: PropsWithChildren) {
-  return <div className="content-focus"> Hello?? {children}</div>;
+  return (
+    <div className={extendsClassName("content-focus", style.container)}>
+      <div className={style.content_nav}>
+        <GoBackComponent />
+        <Link className={style.house} href="/">
+          <HouseIcon />
+        </Link>
+      </div>
+      <section>{children}</section>
+    </div>
+  );
 }
