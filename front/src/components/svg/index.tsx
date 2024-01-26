0dd0f44cb957fb0e4773b0ce65a3fd433ac59260
Ru-kko <verase2003.1@gmail.com>
2024-01-26 17:25:04 -0500

    feat(front): add routing icons
    - Home
    - Go back

--- front/src/components/svg/index.tsx
+++ front/src/components/svg/index.tsx
@@ -305,3 +305,19 @@ export function LinuxTux({ className }: classNameProp) {
     </svg>
   );
 }
+
+export function HouseIcon({ className }: classNameProp) {
+  return (
+    <svg viewBox="0 0 24 24" className={className}>
+      <path d="M2.168,10.555a1,1,0,0,1,.278-1.387l9-6a1,1,0,0,1,1.11,0l9,6A1,1,0,0,1,21,11H19v9a1,1,0,0,1-1,1H6a1,1,0,0,1-1-1V11H3l.019-.019A.981.981,0,0,1,2.168,10.555Z" />
+    </svg>
+  );
+}
+
+export function ReturnIcon({ className }: classNameProp) {
+  return (
+    <svg className={className} viewBox="0 0 32 32">
+      <path d="M0 21.984q0.032-0.8 0.608-1.376l4-4q0.448-0.48 1.056-0.576t1.12 0.128 0.864 0.736 0.352 1.12v1.984h18.016q0.8 0 1.408-0.576t0.576-1.408v-8q0-0.832-0.576-1.408t-1.408-0.608h-16q-0.736 0-1.248-0.416t-0.64-0.992 0-1.152 0.64-1.024 1.248-0.416h16q2.464 0 4.224 1.76t1.76 4.256v8q0 2.496-1.76 4.224t-4.224 1.76h-18.016v2.016q0 0.64-0.352 1.152t-0.896 0.704-1.12 0.096-1.024-0.544l-4-4q-0.64-0.608-0.608-1.44z" />
+    </svg>
+  );
+}
