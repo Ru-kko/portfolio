d433f7e4fd943a94a56a17177e08edf633f3a83b
Ru-kko <verase2003.1@gmail.com>
2024-02-06 14:33:33 -0500

    feat(front):  Cms comunications service

---
+++ front/src/services/impl/GraphqlApiService.ts
@@ -0,0 +1,47 @@
+import type { ListResponse, MessageRequestArgs, ProyectBase, id, Education, EducationBase } from "@/types";
+import type { ApiService } from "..";
+import {
+  getEducationList,
+  getFullEducation,
+  getProtyectsList,
+  postMessage,
+} from "@/lib/graphql";
+
+export class GraphqlApiService implements ApiService {
+  private getVoidListResponse<T>(page?: number): ListResponse<T> {
+    return {
+      docs: [] as T[],
+      page: page ?? 1,
+      prevPage: page ? page - 1 : null,
+      totalPages: 0,
+    };
+  }
+
+  async getFullEducationInfo(id: string): Promise<Education> {
+    const res = await getFullEducation({ id });
+
+    if (res) return res;
+
+    throw new ReferenceError(`Cannot find a Education with id: ${id}`);
+  }
+
+  async getShortProyect(
+    page?: number | undefined,
+  ): Promise<ListResponse<ProyectBase>> {
+    const res = await getProtyectsList({ page });
+
+    return res ?? this.getVoidListResponse();
+  }
+
+  async sendMessage(msg: MessageRequestArgs): Promise<id> {
+    const res = await postMessage({ msg });
+
+    return res as id;
+  }
+
+  async getShortEducation(page?: number): Promise<ListResponse<EducationBase>> {
+    const res = await getEducationList({ page: page ?? 1 });
+
+    return res ?? this.getVoidListResponse();
+  }
+}
