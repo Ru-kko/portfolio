935423545ae26f139863ab9b713ada0a7512061b
Ru-kko <verase2003.1@gmail.com>
2024-03-03 07:29:49 -0500

    fix(front): build without enviroments

--- front/src/utils/enviroment.ts
+++ front/src/utils/enviroment.ts
@@ -1,6 +1,6 @@
 export const GRAPHQL_HOST: string =
-  process.env.GRAPHQL_HOST ?? import.meta.env.GRAPHQL_HOST;
+  process.env.GRAPHQL_HOST ?? import.meta.env.GRAPHQL_HOST ?? "";
 export const STATIC_PATH: string =
-  process.env.STATIC_PATH ?? import.meta.env.STATIC_PATH;
+  process.env.STATIC_PATH ?? import.meta.env.STATIC_PATH ?? "";
 export const APP_DOMAIN: string =
-  process.env.APP_DOMAIN ?? import.meta.env.APP_DOMAIN;
+  process.env.APP_DOMAIN ?? import.meta.env.APP_DOMAIN ?? "";
