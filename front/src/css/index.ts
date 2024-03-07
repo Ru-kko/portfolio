aa83ae82df9681bce2d410837d8de1aa9014f07d
Ru-kko <verase2003.1@gmail.com>
2024-03-06 19:37:02 -0500

    feat(front): 404 page desing

--- front/src/css/index.ts
+++ front/src/css/index.ts
@@ -1,2 +1,3 @@
 export { default as Home } from "./index.module.css";
 export { default as DetailedProyect } from "./detaileProyect.module.css";
+export { default as ErrorPageStyle } from "./404.module.css";
