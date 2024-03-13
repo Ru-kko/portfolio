6d4196155cbe92307beb9c178b4d618bf410459b
Ru-kko <verase2003.1@gmail.com>
2024-03-13 12:48:52 -0500

    fix(front): opengraph url visualization

--- front/src/utils/enviroment.ts
+++ front/src/utils/enviroment.ts
@@ -1,6 +1,6 @@
 export const GRAPHQL_HOST: string =
   process.env.GRAPHQL_HOST ?? import.meta.env.GRAPHQL_HOST ?? "";
-export const STATIC_PATH: string =
-  process.env.STATIC_PATH ?? import.meta.env.STATIC_PATH ?? "";
 export const APP_DOMAIN: string =
-  process.env.APP_DOMAIN ?? import.meta.env.APP_DOMAIN ?? "";
+    process.env.APP_DOMAIN ?? import.meta.env.APP_DOMAIN ?? "https://rukko.pro";
+export const STATIC_PATH: string =
+  process.env.STATIC_PATH ?? import.meta.env.STATIC_PATH ?? APP_DOMAIN;
