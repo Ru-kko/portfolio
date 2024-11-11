f34650e334a4a78557c37b091549330a8157c545
Ru-kko <verase2003.1@gmail.com>
2024-11-10 19:24:17 -0500

    feat: add detailed experience styles

--- front/src/css/index.ts
+++ front/src/css/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from "./index.module.css";
+export { default as DetailedExperience } from "./detailedExperience.module.css";
 export { default as DetailedProyect } from "./detaileProyect.module.css";
 export { default as ErrorPageStyle } from "./404.module.css";
