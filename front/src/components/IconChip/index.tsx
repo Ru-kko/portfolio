f25c1cad04c0b29a6854dd2e4d573945ea25427c
Ru-kko <verase2003.1@gmail.com>
2025-10-11 22:34:00 -0500

    refactor(iconchip): remove debug logging from IconChip component

--- front/src/components/IconChip/index.tsx
+++ front/src/components/IconChip/index.tsx
@@ -107,7 +107,6 @@ const ChipDef: def = {
 
 export function IconChip({ name }: iconProps) {
   const Component = ChipDef[name]?.Icon;
-  console.log({ name, ChipDef, obj: ChipDef[name] });
 
   return (
     <div
